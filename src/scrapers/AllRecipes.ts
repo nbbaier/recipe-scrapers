7e912d02f70feae19a1c1eaab4f568e6d948e692
Nico Baier <nico.baier@gmail.com>
2025-07-05 20:50:22 -0500

    feat(scrapers): update AllRecipes scraper implementation

--- src/scrapers/AllRecipes.ts
+++ src/scrapers/AllRecipes.ts
@@ -7,8 +7,48 @@ export class AllRecipes extends AbstractScraper {
 	}
 
 	host(): string {
-		const element = this.$("allrecipes.com").first();
-		return this.normalize(element.text());
+		return AllRecipes.host();
+	}
+
+	instructions_list(): string[] {
+		const value = this.instructions();
+		return this.normalizeOutput(value, "instructions_list");
+	}
+
+	toJSON() {
+		// biome-ignore lint/suspicious/noExplicitAny: temp
+		const result: any = {};
+		result.author = this.author ? this.author() : null;
+		result.canonicalUrl = this.canonicalUrl ? this.canonicalUrl() : null;
+		result.siteName = this.siteName ? this.siteName() : null;
+		result.host = this.host();
+		result.language = this.language ? this.language() : null;
+		result.title = this.title();
+		result.ingredients = this.ingredients();
+		// result.ingredientGroups = this.ingredientGroups();
+		result.instructions = this.instructions();
+		result.instructions_list = this.instructions_list
+			? this.instructions_list()
+			: null;
+		result.category = this.category ? this.category() : null;
+		result.yields = this.yields();
+		result.description = this.description ? this.description() : null;
+		result.totalTime = this.totalTime ? this.totalTime() : null;
+		result.cookTime = this.cookTime ? this.cookTime() : null;
+		result.prepTime = this.prepTime ? this.prepTime() : null;
+		result.cuisine = this.cuisine ? this.cuisine() : null;
+		// result.cookingMethod = this.cookingMethod ? this.cookingMethod() : null;
+		result.ratings = this.rating ? this.rating() : null;
+		result.ratingsCount = this.reviewCount ? this.reviewCount() : null;
+		// result.equipment = this.equipment ? this.equipment() : null;
+		// result.reviews = this.reviews ? this.reviews() : null;
+		// result.nutrients = this.nutrients ? this.nutrients() : null;
+		// result.dietary_restrictions = this.dietary_restrictions
+		//    ? this.dietary_restrictions()
+		//    : null;
+		result.image = this.image();
+		// result.keywords = this.keywords ? this.keywords() : null;
+		return result;
 	}
 
 	protected titleFromSelector(): string {
